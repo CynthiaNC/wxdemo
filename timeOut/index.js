b06ac97dc7103686a004dfb471915b84d9ed0a13
cynthia <397395892@qq.com>
2016-12-01 13:57:28 +0800

    整理好了代码，改变了用秒数设置

--- timeOut/index.js
+++ timeOut/index.js
@@ -3,24 +3,14 @@ var CANVAS_HEIGHT=400;
 var MARGIN_TOP=50;
 var MARGIN_LEFT=50;
 var RADIUS = 5;
-var DATE_RADIUS=1;
-var endDate = new Date(2016,11,1,12,19,10);
-var showDate='';
+var CIRCLE_DIV = RADIUS + 1;//包含源点的正方形 1.3作为间隙
+var endDate = new Date(2016,11,3,12,19,10);//这个不要了，要秒数
+ var numOrder = ['hour/10','hour%10',':','minutes/10','minutes%10',':','seconds/10','seconds%10'];
+var m={}; //记录每个数字的margin-left
 var currentShow = 0;
-var colors_s = '0123456789ABCDEF';
-var colors=['rgba(249, 134, 149, 0.8)','rgba(247, 145, 234, 0.8)','rgba(214, 126, 246, 0.8)','rgba(36, 240, 70, 0.8)','rgba(102, 244, 240, 0.8)','rgba(227, 193, 251, 0.8)'];
-//var colors=['rgb(249, 134, 149)','rgb(247, 145, 234)','rgb(214, 126, 246)','rgb(36, 240, 70)','rgb(102, 244, 240)','rgb(227, 193, 251)'];
-
-var tangram = [
-    {p:[{x:0,y:0},{x:80,y:0},{x:40,y:40}],color:'#caff67'},
-    {p:[{x:0,y:0},{x:40,y:40},{x:0,y:80}],color:'#67beef'},
-    {p:[{x:80,y:0},{x:80,y:40},{x:60,y:60},{x:60,y:20}],color:'#cf3d61'},
-    {p:[{x:60,y:20},{x:60,y:60},{x:40,y:40}],color:'#f9f51a'},
-    {p:[{x:40,y:40},{x:60,y:60},{x:40,y:80},{x:20,y:60}],color:'#a594c0'},
-    {p:[{x:20,y:60},{x:40,y:80},{x:0,y:80}],color:'#fa8ccc'},
-    {p:[{x:80,y:40},{x:80,y:80},{x:40,y:80}],color:'#f6ca29'}
-]
 var balls = [];
+var secondsTime = 3;
+
 
 window.onload = function () {
     var canvas = document.getElementById('canvas');
@@ -29,14 +19,25 @@ window.onload = function () {
     canvas.width = CANVAS_WIDTH;
     canvas.height = CANVAS_HEIGHT;
 
-    currentShow=getTime();
-    setInterval(function(){
+    var timing = setInterval(function(){
         context.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
         
         render(currentShow,context);
         upate(context);
         upateBall(context);
+        if(secondsTime==0){
+               setTimeout(function(){
+                    clearInterval(timing)
+               },9000)
+            }
+       
     },50)
+    var time = setInterval(function(){
+        secondsTime--;
+               if(secondsTime==-1){
+                clearInterval(time);
+        }
+    },1000)
 }
 
 
@@ -73,19 +74,20 @@ function upate(context){
     var now_minutes=parseInt((now-now_hour*3600)/60);
     var now_seconds=now%60;
 
+
     if(seconds !=now_seconds ){
-        if(parseInt(seconds%10) != parseInt(now_seconds%10))
-            addBall(MARGIN_LEFT + (1+(5*7+8)*2)*(RADIUS+1.2),MARGIN_TOP,seconds%10,context);
-        if(parseInt(hour%10) != parseInt(now_hour%10))
-            addBall(MARGIN_LEFT + (1+1*7*2)*(RADIUS+1.2) ,MARGIN_TOP,hour%10,context);
-        if(parseInt(minutes/10) != parseInt(now_minutes/10))
-            addBall(MARGIN_LEFT + (1+(2*7+4)*2)*(RADIUS+1.2) ,MARGIN_TOP,parseInt(minutes/10),context);
-        if(parseInt(minutes%10) != parseInt(now_minutes%10))
-            addBall(MARGIN_LEFT + (1+(3*7+4)*2)*(RADIUS+1.2) ,MARGIN_TOP,minutes%10,context);
-        if(parseInt(seconds/10) != parseInt(now_seconds/10))
-            addBall(MARGIN_LEFT + (1+(4*7+8)*2)*(RADIUS+1.2) ,MARGIN_TOP,parseInt(seconds/10),context);
         if(parseInt(hour/10) != parseInt(now_hour/10))
-            addBall(MARGIN_LEFT,MARGIN_TOP,seconds%10,context);
+            addBall( m['hour/10'], MARGIN_TOP, seconds%10,context);
+        if(parseInt(hour%10) != parseInt(now_hour%10))
+            addBall( m['hour%10'], MARGIN_TOP, hour%10,context);
+        if(parseInt(minutes/10) != parseInt(now_minutes/10))
+            addBall( m['minutes/10'], MARGIN_TOP, parseInt(minutes/10),context);
+        if(parseInt(minutes%10) != parseInt(now_minutes%10))
+            addBall( m['minutes%10'], MARGIN_TOP, minutes%10,context);
+        if(parseInt(seconds/10) != parseInt(now_seconds/10))
+            addBall( m['seconds/10'], MARGIN_TOP, parseInt(seconds/10),context);
+        if(parseInt(seconds%10) != parseInt(now_seconds%10))
+            addBall( m['seconds%10'], MARGIN_TOP, seconds%10,context);
         currentShow = now;
     }
 
@@ -94,36 +96,27 @@ function upate(context){
 
 function addBall(x,y,num,cxt){
     for (var i = 0; i < digit[num].length ; i++) {
-        for (var j=0;j<digit[num][i].length;j++) {
-            if(digit[num][i][j] == 1){
-                var ballColor;//原本产生颜色，但是有这个就不用啦(~~(Math.random()*(1<<24))).toString(16)
-                for (var c = 0,ballColor='#';c < 6; c++) {
-                            ballColor +=colors_s[Math.floor(Math.random()*colors_s.length)];
-               }
+        for (var j = 0; j < digit[num][i].length; j++) {
+            if( digit[num][i][j] == 1 ){
                 var ball = {
-                    x:x+(RADIUS+1)*j*2,
-                    y:y+ (RADIUS+1)*i*2,
-                    vx:Math.pow(-1,Math.ceil(Math.random()*1000))*4,
-                    vy:-5,
-                    g:1.5+Math.random(),
-                    color:'#'+(~~(Math.random()*(1<<24))).toString(16)//colors[Math.floor(Math.random()*colors.length)]
+                    x: x + CIRCLE_DIV * j * 2,
+                    y: y + CIRCLE_DIV * i * 2,
+                    vx: Math.pow(-1,Math.ceil(Math.random()*1000))*4,
+                    vy: -5,
+                    g: 1.5+Math.random(),
+                    color: '#'+(~~(Math.random()*(1<<24))).toString(16)
                 }
                 balls.push(ball);
-
-                cxt.beginPath();
-                cxt.fillStyle=ball.color;
-                cxt.arc(ball.x, ball.y, RADIUS, 0, 2*Math.PI); //要乘以2，因为计算的应该是直径
-                cxt.fill();
-                cxt.closePath();
             }
         }
     }
 }
 function getTime(){
-        var current = new Date();
-        var ret = endDate.getTime() - current.getTime();
-        ret = Math.round(ret/1000);
-        return ret >= 0 ? ret : 0;
+        // var current = new Date();
+        // var ret = endDate.getTime() - current.getTime();
+        // ret = Math.round(ret/1000);
+        // return ret >= 0 ? ret : 0;
+        return secondsTime >= 0 ? secondsTime : 0;
 }
 
 
@@ -134,39 +127,44 @@ function render(ms,cxt){
     var seconds=ms%60;
 
     //画一个数字的小球
-    //renderDigit(MARGIN_LEFT + 7*(RADIUS+1),MARGIN_TOP,hour%10,cxt);
-    renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hour/10),cxt);
-    renderDigit(MARGIN_LEFT  + (1+ 1*7*2)*(RADIUS+1.2) ,MARGIN_TOP,hour%10,cxt);
-    renderDigit(MARGIN_LEFT  + (1+ 2*7*2)*(RADIUS+1.2) ,MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT  + (1+ (2*7+4)*2)*(RADIUS+1.2) ,MARGIN_TOP,parseInt(minutes/10),cxt);
-    renderDigit(MARGIN_LEFT  + (1+ (3*7+4)*2)*(RADIUS+1.2) ,MARGIN_TOP,minutes%10,cxt);
-    renderDigit(MARGIN_LEFT  + (1+ (4*7+4)*2)*(RADIUS+1.2) ,MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT  + (1+ (4*7+8)*2)*(RADIUS+1.2) ,MARGIN_TOP,parseInt(seconds/10),cxt);
-    renderDigit(MARGIN_LEFT  + (1+ (5*7+8)*2)*(RADIUS+1.2) ,MARGIN_TOP,seconds%10,cxt);
-
-
-    for(var t=MARGIN_TOP+150,l=MARGIN_LEFT,s=0;s<showDate.length;s++){
-        if(showDate[s] == '/')
-            renderDigit( l, t, 11, cxt);
-        else
-            renderDigit( l, t, showDate[s], cxt);
-        l = l+(7*2)*(RADIUS+2);
+    
+    //var num = [hour/10,hour%10,':',minutes/10,minutes%10,':',seconds/10,seconds%10];
+   
+    var num = {
+        'hour/10' : hour/10,
+        'hour%10' : hour%10,
+        ':' : ':',
+        'minutes/10' : minutes/10,
+        'minutes%10' : minutes%10,
+        ':' : ':',
+        'seconds/10' : seconds/10,
+        'seconds%10' : seconds%10
     }
-
+    for(var i = 0, top = MARGIN_TOP, left = MARGIN_LEFT; i < numOrder.length; i++){
+        var isCharacter = false;
+        if(numOrder[i] == ':'){
+            renderDigit( left, top, 10, cxt);
+            isCharacter = true;
+        }else{
+            renderDigit( left, top, parseInt( num[numOrder[i]] ), cxt);
+            isCharacter = false;
+        }
+        if(!m[ numOrder[i] ]) m[ numOrder[i] ] = left;
+        left += 2 * CIRCLE_DIV * ( isCharacter ? 4 : 8);  // 1.3 用来隔离间距 4和7是因为数字是7x10矩阵 ，冒号：是4x10矩阵
+    }
 }
 
 //画一个小球
-function renderDigit(x,y,num,cxt){
+function renderDigit(x,y,num,cxt,color){
     for (var i = 0; i < digit[num].length ; i++) {
         for (var j=0;j<digit[num][i].length;j++) {
             if(digit[num][i][j] == 1){
                 cxt.beginPath();
-                cxt.fillStyle="red";
-                cxt.arc(x+(RADIUS+1)*j*2, y+ (RADIUS+1)*i*2, RADIUS, 0, 2*Math.PI); //要乘以2，因为计算的应该是直径
+                cxt.fillStyle="red"||color;
+                cxt.arc( x + CIRCLE_DIV * j * 2, y + CIRCLE_DIV * i * 2, RADIUS, 0, 2*Math.PI); //要乘以2，因为计算的应该是直径
                 cxt.fill();
                 cxt.closePath();
             }
         }
     }
-
 }
